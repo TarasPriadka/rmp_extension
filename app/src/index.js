9962965165d83f4ba392dc4ddb380fea1f088fab
TarasPriadka[D <taras.priadka@gmail.com>
2020-04-11 19:36:52 -0700

    Moved files

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './componets/App.jsx';
+import App from './js/componets/App.jsx';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
