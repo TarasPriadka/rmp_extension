7e6e8ba1ed9d00f6e6af41bc96eac77f5af46ed6
Taras Priadka <taras.priadka@gmail.com>
2021-04-18 13:36:02 -0700

    Scrape logging (#4)
    * Apparantly you don't need inits anymore
    
    * Added logging while scraping

--- web/src/rmp/scraper/rmp_scraper/spiders/rmp_spider.py
+++ web/src/rmp/scraper/rmp_scraper/spiders/rmp_spider.py
@@ -1,6 +1,6 @@
-import json
 import scrapy
-import logging
+from rmp.utils.logging.rmp_logger import logging #custom logging which saves all the loggin into a txt file
+import os
 from pprint import pprint
 
 from .rmp_parser import url_parser as url_parser
@@ -9,56 +9,37 @@ from .rmp_parser import review_parser as review_parser
 from rmp.utils.sqlite.database import SqlConnector
 from rmp.models.models import Teacher
 
-log = logging.getLogger(__name__)
-
+logging.root.setLevel(logging.DEBUG)
 
 class RMPSpider(scrapy.Spider):
     name = "rmp"
-    names = []
-    count = 0
+    names = ['Manish Goel', 'Julie Wilson']
 
     def __init__(self, *args, **kwargs):
         super(RMPSpider, self).__init__(*args, **kwargs)
-        self.sql = SqlConnector(kwargs['db_path'], kwargs['table_name'])
-        self.names = ["Manish Goel"]
-        # log.debug("Initialized RMP spider...")
-        print('Initialized RMP spider...')
+        db_path = os.path.join(os.environ['DATAROOT'],'db',kwargs['db_file'])
+        self.sql = SqlConnector(db_path, kwargs['table_name'])
+        logging.info(f"Initialized RMP spider with db at {db_path}...")
 
     def start_requests(self):
-        # for name in self.names[0]:
-        # url = url_parser.create_rmp_url(name,'De Anza')
-        # print(f"Parsing url: {url}")
-        # yield scrapy.Request(url=url, callback=self.parseRMP)
-        yield scrapy.Request('https://www.ratemyprofessors.com/ShowRatings.jsp?tid=1836438', callback=self.parse_teachers)
+        for name in self.names:
+            url = url_parser.create_rmp_url(name,'De Anza')
+            logging.debug(f"Parsing url: {url}")
+            yield scrapy.Request(url=url, callback=self.parseRMP)
 
-    def parseRMP(self, response):
-        # urls = response.xpath(
-        # "//body//ul[contains(@class,'listing')]//@href").getall()
-        # print("Got response with urls: ",urls)
-        # urls = ["https://www.ratemyprofessors.com" + url for url in urls]
-        urls = ['https://www.ratemyprofessors.com/ShowRatings.jsp?tid=1836438']
+    def parseRMP(self, response:scrapy.Request):
+        urls = response.xpath("//body//ul[contains(@class,'listing')]//@href").getall()
+        urls = ["https://www.ratemyprofessors.com" + url for url in urls]
+        logging.debug(f'Response from {response.url}, urls: [{urls}]')
         for url in urls:
-            yield scrapy.Request(url, callback=self.parse_teachers)
-
-    def parseGoogle(self, response):
-        links = response.xpath("//body/div//@href").getall()
-        links = [url if 'ratemyprofessor' in url else '' for url in links]
-        links = list(filter(lambda i: i != '' and 'ShowRatings' in i, links))
-        for link in links:
-            url = url_parser.reconstruct_url(link)
             yield scrapy.Request(url, callback=self.parse_teachers)
 
     def parse_teachers(self, response):
         # Header
         teacher_info = {}
-        print("RESPONSE: ", response)
         teacher_info['header'] = review_parser.parse_header(response)
         teacher_info['reviews'] = review_parser.parse_review(response)
-        # print("Got teacher info: ", teacher_info)
-        # write_json(teacher_info)
-        pprint(teacher_info)
-
         teacher = Teacher.parse_dict(teacher_info)
         self.sql.insert(teacher)
-
+        logging.debug(f'Successfully parsed teacher: {teacher.last} {teacher.first}')
         yield teacher
