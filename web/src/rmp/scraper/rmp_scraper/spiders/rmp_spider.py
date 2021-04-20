904ff9a7b84c692f83d8a0cfba63449f883e6d67
TarasPriadka <taras.priadka@gmail.com>
2021-04-20 00:18:07 -0700

    Updated spider and added data folder setup script

--- web/src/rmp/scraper/rmp_scraper/spiders/rmp_spider.py
+++ web/src/rmp/scraper/rmp_scraper/spiders/rmp_spider.py
@@ -1,5 +1,7 @@
 import scrapy
-from rmp.utils.logging.rmp_logger import logging #custom logging which saves all the loggin into a txt file
+import json
+# custom logging which saves all the loggin into a txt file
+from rmp.utils.logging.rmp_logger import logging
 import os
 from pprint import pprint
 
@@ -11,24 +13,37 @@ from rmp.models.models import Teacher
 
 logging.root.setLevel(logging.DEBUG)
 
+
 class RMPSpider(scrapy.Spider):
     name = "rmp"
-    names = ['Manish Goel', 'Julie Wilson']
 
     def __init__(self, *args, **kwargs):
         super(RMPSpider, self).__init__(*args, **kwargs)
-        db_path = os.path.join(os.environ['DATAROOT'],'db',kwargs['db_file'])
-        self.sql = SqlConnector(db_path, kwargs['table_name'])
-        logging.info(f"Initialized RMP spider with db at {db_path}...")
+        input_path = os.path.join(
+            os.environ['DATAROOT'], 'scraping', kwargs['input_file'])
+
+        with open(input_path) as fp:
+            spider_input = json.load(fp)
+
+        self.college = spider_input['college']
+        self.names = spider_input['names']
+        logging.info(f'Running scraping job on school {self.college}. Got names: {self.names}')
+
+        db_path = os.path.join(os.environ['DATAROOT'], 'db', kwargs['db_file'])
+        
+        self.sql = SqlConnector(db_path, spider_input['table_name'])
+        logging.info(f'Initialized RMP spider with db at {db_path}, and table name {spider_input["table_name"]}')
+        
 
     def start_requests(self):
         for name in self.names:
-            url = url_parser.create_rmp_url(name,'De Anza')
+            url = url_parser.create_rmp_url(name, 'De Anza')
             logging.debug(f"Parsing url: {url}")
             yield scrapy.Request(url=url, callback=self.parseRMP)
 
-    def parseRMP(self, response:scrapy.Request):
-        urls = response.xpath("//body//ul[contains(@class,'listing')]//@href").getall()
+    def parseRMP(self, response: scrapy.Request):
+        urls = response.xpath(
+            "//body//ul[contains(@class,'listing')]//@href").getall()
         urls = ["https://www.ratemyprofessors.com" + url for url in urls]
         logging.debug(f'Response from {response.url}, urls: [{urls}]')
         for url in urls:
@@ -41,5 +56,6 @@ class RMPSpider(scrapy.Spider):
         teacher_info['reviews'] = review_parser.parse_review(response)
         teacher = Teacher.parse_dict(teacher_info)
         self.sql.insert(teacher)
-        logging.debug(f'Successfully parsed teacher: {teacher.last} {teacher.first}')
+        logging.debug(
+            f'Successfully parsed teacher: {teacher.last} {teacher.first}')
         yield teacher
---
+++ web/src/rmp/utils/general.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+import json
+
+def setup_dataroot():
+    dataroot = os.environ['DATAROOT']
+    Path(dataroot).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(dataroot,'logging')).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(dataroot,'scraping')).mkdir(parents=True, exist_ok=True)
+    with open(os.path.join(dataroot,'scraping','scrape_input.json'),'w') as fp:
+        d = {
+            'college': '',
+            'names': [],
+            'table_name': '',
+        }
+        json.dump(d,fp,indent=4)
+
+if __name__ == '__main__':
+    setup_dataroot()
